088f5df7f7249024b3de94dd2638b0b5a540397d
TomasUri <urinovic.tomas@gmail.com>
2022-05-05 11:52:34 +0300

    Paskaita

---
+++ labas.php
@@ -0,0 +1,44 @@
+<h1>Pats tu labas!</h1>
+
+<?php
+
+echo 'rasau ka noriu';
+echo 66 + 10;
+
+?> 
+
+<h1>Pats tu labas!</h1>
+
+<?php
+
+echo 'rasau ka noriu';
+echo 66 + 10;
+
+?> 
+
+<h1>Pats tu labas!</h1>
+
+<?php
+
+$labas = 'valio';
+echo $labas. "\n";
+echo $labas;
+echo $labas. "\n";
+echo '<br>';
+print_r($labas);
+
+$a = 5;
+echo $a++ + ++$a;
+echo '<br>';
+echo ++$a + $a++;
+// kad sudeti stringus
+// paprastas stringas visada viengubos kabutes
+$a = 6;
+echo '<br>';
+echo 'labas ' . 'rytas';
+echo '<br>';
+// dvigubos kabutes naudojamos kintamajam irasyti ir spec. zenklams
+echo "labas $a";
+// \n = new line
+echo '<br>';
+echo "labas \n $a";
